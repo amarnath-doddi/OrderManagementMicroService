e9cb1621a77e0217be36aa3edd710a565f892b9c
amarnath-doddi <31587643+amarnath-doddi@users.noreply.github.com>
2021-06-12 03:18:44 +0530

    Add files via upload

---
+++ src/main/java/com/example/ecom/order/OrderManagementMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecom.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecom/order/OrderManagementMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecom.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
